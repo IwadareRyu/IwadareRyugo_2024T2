3b82216321b17b1d364a8aab8f4aa508da69ebc6
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2024-07-06 18:30:54 +0900

    [Add]Playerを動かす機能の追加

---
+++ IwadareRyugo_2024_T2/IwadareRyugo_2024_T2/Player.cpp
@@ -0,0 +1,63 @@
+﻿#include "stdafx.h"
+#include "Player.h"
+
+Player::Player() : m_currentSpeed(player::DEFAULT_SPEED),m_position(player::INIT_POS),m_isPlayerFacingRight(false){}
+
+
+
+void Player::M_PlayerInput(){
+	if (KeyLeft.pressed() || KeyA.pressed()){
+		M_PlayerXMove(-1);
+		
+	}
+	else if (KeyRight.pressed() || KeyD.pressed()){
+		M_PlayerXMove(1);
+	}
+
+	if (KeyUp.pressed() || KeyW.pressed()){
+		M_PlayerYMove(-1);
+	}
+	else if (KeyDown.pressed() || KeyS.pressed()){
+		M_PlayerYMove(1);
+	}
+
+	if (KeySpace.pressed())
+	{
+		M_PlayerShot();
+	}
+}
+
+void Player::M_PlayerXMove(int minas){
+	m_position.x = m_position.x + minas * m_currentSpeed * Scene::DeltaTime();
+	if(minas < 0){
+		m_position.x = Max(m_position.x, player::MIN_POS.x);
+		m_isPlayerFacingRight = true;
+	}
+	else {
+		m_position.x = Min(m_position.x, player::MAX_POS.x);
+		m_isPlayerFacingRight = false;
+	}
+}
+
+void Player::M_PlayerYMove(int minas){
+	m_position.y = m_position.y + minas * m_currentSpeed * Scene::DeltaTime();
+	if (minas < 0) {
+		m_position.y = Max(m_position.y, player::MIN_POS.y);
+	}
+	else {
+		m_position.y = Min(m_position.y, player::MAX_POS.y);
+	}
+}
+
+void Player::M_PlayerShot(){
+
+}
+
+void Player::M_PlayerLife(){
+
+}
+
+void Player::M_PlayerDraw(double scale)
+{
+	m_playerTexture.scaled(scale).mirrored(m_isPlayerFacingRight).drawAt(m_position);
+}
---
+++ IwadareRyugo_2024_T2/IwadareRyugo_2024_T2/Player.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+namespace player
+{
+	const int DEFAULT_SPEED = 200;
+
+	const Vec2 INIT_POS{ 400,400 };
+
+	const Vec2 MIN_POS = { 60.0 ,30.0 };
+
+	const Vec2 MAX_POS = { 740.0,550.0 };
+
+
+}
+
+class Player
+{
+public:
+	int m_currentSpeed;
+
+	Vec2 m_position;
+
+	bool m_isPlayerFacingRight;
+
+	Texture m_playerTexture;
+
+	Player();
+
+	~Player() {}
+
+	void M_PlayerInput();
+
+	void M_PlayerXMove(int minas);
+
+	void M_PlayerYMove(int minas);
+
+	void M_PlayerShot();
+
+	void M_PlayerLife();
+
+	void M_PlayerDraw(double scale);
+};
+
